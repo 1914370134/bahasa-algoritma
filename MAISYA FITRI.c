ee54f59c2a040e728821cf4c32aef9a380a4c276
maisya fitri anugrah <55613695+1914370134@users.noreply.github.com>
2019-10-21 01:49:21 -0700

    Add files via upload

---
+++ MAISYA FITRI.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tBIODATA PRIBADI\n\n");
+    printf("NAMA     : MAISYA FITRI ANUGRAH\n");
+    printf("NPM      : 1914370134\n");
+    printf("KELAS    : REGULER 1D\n");
+    printf("PRODI    : SISTEM KOMPUTER\n");
+    printf("FAKULTAS :SAINS & TEKNOLOGI\n");
+
+
+    int a=20;
+    int b=10;
+    int c;
+    c=a+b;
+    printf("baris 1 -nilai c adalah %d\n",c);
+    c=a-b;
+    printf("baris 2 -nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 -nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 4 -nilai c adalah %d\n",c);
+    c= b%a;
+    printf("baris 5 -nilai c adalah %d\n",c);
+    return 0;
+}
