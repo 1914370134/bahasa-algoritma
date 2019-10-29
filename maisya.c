7a7aacc5fa772568287cc7bbff2b4804a13d7e79
maisya fitri anugrah <55613695+1914370134@users.noreply.github.com>
2019-10-28 23:50:12 -0700

    Add files via upload

---
+++ maisya.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int x,y,z;
+
+x = 20;
+y = 10;
+
+z=x+y;
+printf("hasil dari penjumlahan %d\n",z);
+z=x-y;
+printf("hasil dari pengurangan %d\n",z);
+z=x*y;
+printf("hasil dari pembagian %d\n",z);
+z=x/y;
+printf("hasil dari pembagian %d\n",z);
+z=x % y;
+printf("hasil dari modulus   %d\n",z);
+return 0;
+}
